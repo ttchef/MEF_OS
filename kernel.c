8299b821ad5bc1449c8d4b5f164e1c9b553180da
ttchef <ruro0.edit@gmail.com>
2025-06-22 10:13:50 +0200

    adds math functions

--- kernel.c
+++ kernel.c
@@ -22,15 +22,13 @@ void main()
 
     while (1) {
         
-        
         start_bench();
 
         clear_color((Color){35,35,35});
 
         u64 time = stop_bench_us();
         printf("Time For BG: %d\n", time);
-        
-
+    
         start_bench();
 
         rc_display();
---
+++ math.h
@@ -0,0 +1,15 @@
+
+#ifndef MATH_H
+#define MATH_H 
+
+#ifndef TYPES_H
+#include "types.h"
+#endif
+
+extern const u8 sine_table[91];
+
+float isin(u16 i);
+float icos(u16 i);
+
+#endif
+
